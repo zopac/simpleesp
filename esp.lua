d3d6c225182eab9696c0c387f4b11d384b04d1b7
yeah <61770151+zopac@users.noreply.github.com>
2022-09-18 21:21:32 -0500

    ALL CREDIT TO BABYHAMSTA

---
+++ esp.lua
@@ -0,0 +1,48 @@
+function Simple_Create(base, name, trackername, color, studs)
+    local bb = Instance.new('BillboardGui', game.CoreGui)
+    bb.Adornee = base
+    bb.ExtentsOffset = Vector3.new(0,1,0)
+    bb.AlwaysOnTop = true
+    bb.Size = UDim2.new(0,6,0,6)
+    bb.StudsOffset = Vector3.new(0,1,0)
+    bb.Name = trackername
+
+    local txtlbl = Instance.new('TextLabel', bb)
+    txtlbl.ZIndex = 10
+    txtlbl.BackgroundTransparency = 1
+    txtlbl.Position = UDim2.new(0,0,0,-48)
+    txtlbl.Size = UDim2.new(1,0,10,0)
+    txtlbl.Font = 'ArialBold'
+    txtlbl.FontSize = 'Size12'
+    txtlbl.Text = name
+    txtlbl.TextStrokeTransparency = 0.5
+    txtlbl.TextColor3 = color
+
+    local txtlblstud = Instance.new('TextLabel', bb)
+    txtlblstud.ZIndex = 10
+    txtlblstud.BackgroundTransparency = 1
+    txtlblstud.Position = UDim2.new(0,0,0,-35)
+    txtlblstud.Size = UDim2.new(1,0,10,0)
+    txtlblstud.Font = 'ArialBold'
+    txtlblstud.FontSize = 'Size12'
+    txtlblstud.Text = tostring(studs) .. " Studs"
+    txtlblstud.TextStrokeTransparency = 0.5
+    txtlblstud.TextColor3 = Color3.new(255,255,255)
+end
+
+-- Clear ESP
+function Simple_ClearESP(espname)
+    for _,v in pairs(game.CoreGui:GetChildren()) do
+        if v.Name == espname and v:isA('BillboardGui') then
+            v:Destroy()
+        end
+    end
+end
+
+getgenv().SESP_Create = Simple_Create;
+getgenv().SESP_Clear = Simple_ClearESP;
+
+-- Example:
+-- Part/Instance, Display Name, Esp Name, Color of ESP, Studs (Can be calculated using Plr:DistanceFromCharacter())
+-- SESP_Create(v.Character.Head, v.Name, "playertracker", Color3, math.floor(studs + 0.5))
+-- SESP_Clear("playertracker");
